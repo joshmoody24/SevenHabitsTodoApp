b25395ee42bb25a5817cafc438f5fa617324a565
Josh Moody <joshmoody24@gmail.com>
2022-02-07 19:22:46 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SevenHabitsTodoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
